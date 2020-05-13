ee1746340a976be6260db51df6e8ee14fd6ae1e0
asuka y <yagi.asuka.0x06@gmail.com>
2020-05-13 23:33:45 +0900

    add templates

--- main.go
+++ main.go
@@ -3,20 +3,34 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-<html>
-	<head>
-		<title>チャット</title>
-	</head>
-	<body>
-		チャットをしましょう!
-	</body>
-</html>`))
+// templは1つのテンプレートを表します
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		// 最初に実行されるときにコンパイルされる
+		// ただし、エラーが発生居売る場合には初期化時に実行される方が好ましい場合もある
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	err := t.templ.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	// ルート
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	// Webサーバーを開始します
 	if err := http.ListenAndServe(":8080", nil); err != nil {

c137862570825711d5e15a2b376670eedc979661
asuka y <yagi.asuka.0x06@gmail.com>
2020-05-14 22:40:16 +0900

    websocket実装

--- room.go
+++ room.go
@@ -1,6 +1,80 @@
 package main
 
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
 type room struct {
 	// forward は他のクライアントに転送するためのメッセージを保持するチャネルです。
 	forward chan []byte
+	// join はチャットルームに参加しようとしているクライアントのためのチャネルです
+	join chan *client
+	// leave はチャットルームから退室しようとしているクライアントのためのチャネルです
+	leave chan *client
+	// clientsには在室している全てのクライアントが保持されています
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			// 退室
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// すべてのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージを送信
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() {
+		r.leave <- client
+	}()
+	go client.write()
+	client.read()
+}
+
+// newRoom はすぐに利用できるチャットルームを生成して返します
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
 }

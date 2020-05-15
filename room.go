e9c7dd828007d99769bb2ebabd2fe38625644c12
asuka y <yagi.asuka.0x06@gmail.com>
2020-05-16 01:02:14 +0900

    traceパッケージの追加

--- room.go
+++ room.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"chat/trace"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
@@ -22,6 +23,8 @@ type room struct {
 	leave chan *client
 	// clientsには在室している全てのクライアントが保持されています
 	clients map[*client]bool
+	// tracerはチャットルーム上で行われた操作のログを受け取ります
+	tracer trace.Tracer
 }
 
 func (r *room) run() {
@@ -30,20 +33,25 @@ func (r *room) run() {
 		case client := <-r.join:
 			// 参加
 			r.clients[client] = true
+			r.tracer.Trace("新しいクライアントが参加しました")
 		case client := <-r.leave:
 			// 退室
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("クライアントが退室しました")
 		case msg := <-r.forward:
+			r.tracer.Trace("メッセージを受信しました: ", string(msg))
 			// すべてのクライアントにメッセージを転送
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					// メッセージを送信
+					r.tracer.Trace(" -- クライアントに送信されました")
 				default:
 					// 送信に失敗
 					delete(r.clients, client)
 					close(client.send)
+					r.tracer.Trace(" -- 送信に失敗しました。クライアントをクリーンアップします")
 				}
 			}
 		}
@@ -76,5 +84,6 @@ func newRoom() *room {
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
+		tracer:  trace.Off(),
 	}
 }

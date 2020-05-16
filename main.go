358b968f4c1443e392f42bd3cf22b07d1c124d6e
asuka y <yagi.asuka.0x06@gmail.com>
2020-05-17 01:46:38 +0900

    add auth

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"flag"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -32,9 +36,18 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() // フラグを解釈
+	// Gomniauthのセットアップ
+	gomniauth.SetSecurityKey(seckey)
+	gomniauth.WithProviders(
+		facebook.New(facebookid, facebooksec, "http://localhost:8080/auth/callback/facebook"),
+		github.New(githubid, githubsec, "http://localhost:8080/auth/callback/github"),
+		google.New(googleid, googlesec, "http://localhost:8080/auth/callback/google"),
+	)
 
 	r := newRoom()
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 	// チャットルームを開始します
 	go r.run()
